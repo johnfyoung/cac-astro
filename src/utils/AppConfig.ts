2d95e1e34764e5279ff633ca3befb2ef5e7d76e5
John F Young <john@codeandcreative.com>
2023-05-20 12:24:53 -0700

    feat(config): adding more GA configuration

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,8 +1,8 @@
 export const AppConfig = {
-  site_name: 'Astro boilerplate',
-  title: 'Astro boilerplate',
-  description: 'Boilerplate built with Astro using React and Tailwind CSS',
-  author: 'Emma',
+  site_name: 'codeandcreative.com',
+  title: 'blogs and such',
+  description: 'A website built with the Astro boilerplate',
+  author: 'John',
   locale_region: 'en-us',
   locale: 'en',
 };
