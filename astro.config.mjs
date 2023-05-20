e2c2244edbb6e2caf7c776a157252cffdbbd8994
John F Young <john@codeandcreative.com>
2023-05-20 12:11:43 -0700

    feat(config): adds configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,12 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 import robotsTxt from 'astro-robots-txt';
 import { astroImageTools } from 'astro-imagetools';
+import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
   // base: '.', // Set a path prefix.
-  site: 'https://example.com/', // Use to generate your sitemap and canonical URLs in your final build.
+  site: 'https://codeandcreative.com', // Use to generate your sitemap and canonical URLs in your final build.
   // Important!
   // Only official '@astrojs/*' integrations are currently supported by Astro.
   // Add 'experimental.integrations: true' to make 'astro-robots-txt' working
@@ -29,5 +30,11 @@ export default defineConfig({
     sitemap(),
     robotsTxt(),
     astroImageTools,
+    partytown({
+      // Adds dataLayer.push as a forwarding-event.
+      config: {
+        forward: ['dataLayer.push'],
+      },
+    }),
   ],
 });
